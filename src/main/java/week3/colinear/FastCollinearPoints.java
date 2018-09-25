6ec72e35894364bbad0e46740837d35b8ad2eaff
Dmytro Pashko <dmytro.pashko@comigo.com>
2018-09-25 16:21:36 +0300

    Implemented calculation points with the same slope to current point.

--- src/main/java/week3/colinear/FastCollinearPoints.java
+++ src/main/java/week3/colinear/FastCollinearPoints.java
@@ -2,6 +2,7 @@ package week3.colinear;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class FastCollinearPoints {
@@ -41,7 +42,47 @@ public class FastCollinearPoints {
 
     private void calculateSegments(Point[] points) {
         segments = new ArrayList<>();
-        //TODO implement this method.
+
+        for (int i = 0; i < points.length - 1; i++) {
+            Point current = points[i];
+            //Sort next points in list by slope according to current point.
+            Point[] pointsBySlope = points.clone();
+            Arrays.sort(pointsBySlope, current.slopeOrder());
+            calculateCollinearPoints(current, pointsBySlope);
+        }
+    }
+
+    /**
+     * Calculate points with the same slope to current point.
+     * Creating Segment when list of points with the same slope have more that 3 items.
+     */
+    private void calculateCollinearPoints(Point current, Point[] pointsSortedBySlope) {
+        List<Point> slopePoints = new ArrayList<>();
+
+        double prevSlope = Double.NEGATIVE_INFINITY;
+
+        for (int j = 1; j < pointsSortedBySlope.length; j++) {
+
+            double slope = current.slopeTo(pointsSortedBySlope[j]);
+            //if slope the same as prev point slope.
+            if (slope == prevSlope) {
+                slopePoints.add(pointsSortedBySlope[j]);
+            } else {
+                if (slopePoints.size() >= 3) {
+                    createSegment(current, slopePoints);
+                }
+                slopePoints.clear();
+                slopePoints.add(pointsSortedBySlope[j]);
+            }
+            prevSlope = slope;
+        }
+        if (slopePoints.size() >= 3) {
+            createSegment(current, slopePoints);
+        }
+    }
+
+    private void createSegment(Point current, List<Point> points) {
+
     }
 
     // the number of line segments
